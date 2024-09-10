763a261dbb05658852ac116b7b1e2fb831453716
nstarman <nstarman@users.noreply.github.com>
2024-09-10 01:16:29 -0400

    feat: add sybil
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ conftest.py
@@ -0,0 +1,15 @@
+"""Doctest configuration."""
+
+from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
+
+from sybil import Sybil
+from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
+
+pytest_collect_file = Sybil(
+    parsers=[
+        DocTestParser(optionflags=NORMALIZE_WHITESPACE | ELLIPSIS),
+        PythonCodeBlockParser(),
+        SkipParser(),
+    ],
+    patterns=["*.rst", "*.py"],
+).pytest()
